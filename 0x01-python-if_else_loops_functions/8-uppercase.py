1efa7c01bc11aa8055ab467c0ee3ca3c6927730b
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-08-08 23:58:32 +0000

    Print a string in uppercase : Refactor

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,12 +2,11 @@
 def uppercase(str):
     lenght: int = len(str)
     i: int = 0
+    if lenght == 0:
+        print("")
     for c in str:
         c_int = ord(c)
         i += 1
-        if c_int >= 65 and c_int <= 90:
-            print("{:s}".format(c), end=("" if i < lenght else "\n"))
-        else:
+        if c_int >= 97 and c_int <= 122:
             c = chr(c_int - 32)
-            print("{:s}".format(c),
-                  end=("" if i < lenght else "\n"))
+        print("{:s}".format(c), end=("" if i < lenght else "\n"))
