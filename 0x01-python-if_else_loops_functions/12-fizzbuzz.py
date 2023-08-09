94b02ef34d43bf63af0696640a943620ec38f4b2
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-08-09 00:37:36 +0000

    Print Fizz Buzz FizzBuzz or the number

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for value in range(1, 101):
+        if value % 15 == 0:
+            result = "FizzBuzz"
+        elif value % 5 == 0:
+            result = "Buzz"
+        elif value % 3 == 0:
+            result = "Fizz"
+        else:
+            result = value
+        print("{}".format(result), end=(" " if value != 100 else "\n"))
