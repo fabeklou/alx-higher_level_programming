7c1f2d0e515a2f4d16c3380c013bba4b72de3e08
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-08-09 00:53:34 +0000

    Compute a to the power of b : Using two asterix

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    result: int = 1
-    if b > 0:
-        while b > 0:
-            b -= 1
-            result *= a
-    elif b < 0:
-        while b < 0:
-            b += 1
-            result /= a
-    return result
+    return a ** b
