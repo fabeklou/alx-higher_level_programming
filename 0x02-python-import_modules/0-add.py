04531a77d68707b8bf144f8a70e70c3cbde41373
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-08-10 23:51:35 +0000

    Import and use a function : not a module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
 
 if __name__ == "__main__":
+    from add_0 import add
     a: int = 1
     b: int = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
