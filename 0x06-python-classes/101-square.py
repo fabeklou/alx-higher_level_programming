e23c5b99d0920ac48070266e8cb9d1e6dcd492eb
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-08-30 20:35:53 +0000

    fix: size=0 handling by returning an empty string

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -23,8 +23,8 @@ class Square:
             ValueError: if size is less than zero (0)
 
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self) -> int:
@@ -109,7 +109,7 @@ class Square:
 
         """
         if self.size == 0:
-            return
+            return ""
         data_str: str = ""
         data_str += self.position[1] * '\n'
         for x in range(self.size):
