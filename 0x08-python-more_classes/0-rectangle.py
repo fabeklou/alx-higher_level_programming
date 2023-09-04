04fc0e3b4568f9fdbb744423e3e07e36afe80ff7
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-09-04 14:39:27 +0000

    feat: define an empty Rectangle class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""This module defines a Rectangle class"""
+
+
+class Rectangle:
+    """This is an empty Rectangle class that defines
+    a basic structure for Rectangle Objects
+
+    """
+    pass
+
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
