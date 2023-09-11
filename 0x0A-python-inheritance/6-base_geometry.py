914413df34a30dd31b092839a59a46cea9a3eb89
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-09-11 13:25:36 +0000

    feat: define the area method in BaseGeometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""This module defines a BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class defines basic methods and
+    attributes to perform geometric task
+
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
