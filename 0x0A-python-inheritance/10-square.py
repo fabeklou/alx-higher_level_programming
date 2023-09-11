e75ff3915eb9a04e117ab59ffb367b44e1a406d0
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-09-11 19:09:27 +0000

    feat: create Square class a Rectangle sub_class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""square module: defines a square class"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square: defines a basic structure for
+    Square objects
+
+    """
+
+    def __init__(self, size):
+        """constructor or the Square class
+
+        Args:
+            size (int): size of the Square object
+
+        Raises:
+            TypeError:
+                if size is not an integer
+            ValueError:
+                if size is a null or negative integer
+
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """area method calculate the area of
+        the rectangle object and return the result
+
+        """
+        return self.__size * self.__size
