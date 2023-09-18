7b6c38d78fcaeaed722eb458b72e2128904c28f0
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-09-18 23:01:32 +0000

    feat: update rectangle class with validation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,7 +12,7 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """constructor of new rectangle objects
-        
+
         Args:
             width (int):
                 width of the rectangle
@@ -28,57 +28,77 @@ class Rectangle(Base):
         self.width = width
         self.height = height
         self.x = x
-        self.y = x
+        self.y = y
 
     @property
     def width(self):
         """The width property can be used to set/get the value
         of the width private instance attribute safely
-        
+
         Args:
             value (int):
                 width of the rectangle object
-        
+
         Returns:
             (int):
                 the width of the rectangle object
+
+        Raises:
+            TypeError:
+                if width is not an integer
+            ValueError:
+                if width is <= 0
 
         """
         return self.__width
 
     @width.setter
     def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
     def height(self):
         """The height property can be used to set/get the value
         of the height private instance attribute safely
-        
+
         Args:
             value (int):
                 height of the rectangle object
-        
+
         Returns:
             (int):
                 the height of the rectangle object
+
+        Raises:
+            TypeError:
+                if height is not an integer
+            ValueError:
+                if height is <= 0
 
         """
         return self.__height
 
     @height.setter
     def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
     def x(self):
         """The x property can be used to set/get the value
         of the x private instance attribute safely
-        
+
         Args:
             value (int):
                 x position of the rectangle object
-        
+
         Returns:
             (int):
                 the x position of the rectangle object
@@ -88,17 +108,21 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
         """The y property can be used to set/get the value
         of the y private instance attribute safely
-        
+
         Args:
             value (int):
                 y position of the rectangle object
-        
+
         Returns:
             (int):
                 the y position of the rectangle object
@@ -108,4 +132,8 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
