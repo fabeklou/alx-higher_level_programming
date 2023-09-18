720cb12b6b1c59d23c7da6808175cee42e707b0e
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-09-18 23:05:12 +0000

    feat: implement the area instance method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -137,3 +137,7 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """returns the area value of a reactangle object"""
+        return self.width * self.height
