56f1d75f54c100d6424f69fe9e0cfcdc7d8a324d
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-10-19 00:57:52 +0000

    fix: typo in value alias avg_temp

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -3,5 +3,5 @@
 SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures`
     WHERE `month` = 7 OR `month` = 8
     GROUP BY `city`
-    ORDER BY `avg_value` DESC
+    ORDER BY `avg_temp` DESC
     LIMIT 3;
