ea10d9d5e14763f66c7568fed6b25192bab3ea0a
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-10-22 20:34:25 +0000

    fix: use inner join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- This script lists all cities contained in the database `hbtn_0d_usa`
 SELECT cities.id, cities.name, states.name
 FROM cities
-    NATURAL JOIN states
+    INNER JOIN states cities.state_id=states.id
     ORDER BY cities.id ASC;
