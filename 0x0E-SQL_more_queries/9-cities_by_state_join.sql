1f2c88ee5b86b5cd179a8476959e2d263d179468
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-10-22 20:37:58 +0000

    fix: add missing ON

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- This script lists all cities contained in the database `hbtn_0d_usa`
 SELECT cities.id, cities.name, states.name
 FROM cities
-    INNER JOIN states cities.state_id=states.id
+    INNER JOIN states ON cities.state_id=states.id
     ORDER BY cities.id ASC;
