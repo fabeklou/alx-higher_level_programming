61e6528d0f967738cdb50ec26795e7e413047bfe
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-10-22 11:16:24 +0000

    fix: typo in identified

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,5 +2,5 @@
 --      `user_0d_1` have all privileges on the MySQL server
 --      The `user_0d_1` password is set to `user_0d_1_pwd`
 --      If the user `user_0d_1` already exists, the script does not fail
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' INDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
