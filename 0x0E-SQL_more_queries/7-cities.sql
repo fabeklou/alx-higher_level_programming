3f3b55198b821ff48a318cb779971a71db2320c5
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-10-22 13:19:31 +0000

    fix: add missing fk declaration

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -8,7 +8,8 @@ CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 USE `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `cities`(
     id INT NOT NULL AUTO_INCREMENT,
-    FOREIGN KEY(state_id) REFERENCES states(id),
+    state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
+    FOREIGN KEY(state_id) REFERENCES states(id),
     PRIMARY KEY(id)
 );
