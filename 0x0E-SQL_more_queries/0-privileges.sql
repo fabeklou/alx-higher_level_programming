19d66424e4620baed29f7f63c5155553f9ede660
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-10-22 10:52:08 +0000

    feat: show privileges granted to a user

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- This script lists all privileges of the MySQL users
+--      `user_0d_1` and `user_0d_2` on a server
+SHOW GRANTS FOR 'user_0d_1'@'localhost'
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
