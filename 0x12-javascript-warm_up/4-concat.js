2408ee0f1207465aea60df0671ff018955702688
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-11-13 23:55:41 +0000

    feat: print two arguments passed in a format

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+/**
+ * This script prints two arguments passed to it, in the following format:
+ *      <argument_one> is <argument_two>
+ */
+console.log(process.argv[2] + ' is ' + process.argv[3]);
