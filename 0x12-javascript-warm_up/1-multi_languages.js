6e2160f62bac576880f0cd0e3115da63fcf7150d
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-11-13 23:53:20 +0000

    feat: print 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/**
+ * This script prints 3 strings, one at the line:
+ *      'C is fun'
+ *      'Python is cool'
+ *      'JavaScript is amazing'
+ */
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
