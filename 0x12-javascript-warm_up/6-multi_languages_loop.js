64ba0f3fb9edc89e77585600a1b3561c67e1139f
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-11-13 23:56:51 +0000

    feat: print 3 lines using an array of string and a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+/**
+ * This script prints 3 lines  using an array of string and a loop
+ */
+const strArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const value of strArray) {
+  console.log(`${value}`);
+}
