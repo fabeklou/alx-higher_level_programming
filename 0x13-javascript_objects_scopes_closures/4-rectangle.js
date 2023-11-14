4eb2bee45b748dd4e9cb3853df135007c7b8099f
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-11-14 22:33:04 +0000

    feat: add rotate & double methods to the rectangle class

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,59 @@
+#!/usr/bin/node
+
+/**
+ * This module defines a Rectangle class, blueprint for
+ * rectangle objects
+ */
+
+module.exports = class Rectangle {
+  /**
+   * The constructor of the Rectangle class - initializes a new
+   * Rectangle object
+   *
+   * @param {number} w - width of the rectangle
+   * @param {number} h - height of the rectangle
+   *
+   * description - a empty Rectangle object is created if
+   * either <w> or <h> is not valid (less or equal to 0)
+   */
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  /**
+   * prints a visual representation of a Rectangle object
+   * using the character 'X'
+   */
+  print () {
+    let line = '';
+
+    for (let x = 0; x < this.width; x++) {
+      line += 'X';
+    }
+
+    for (let y = 0; y < this.height; y++) {
+      console.log(line);
+    }
+  }
+
+  /**
+   * exchanges the <width> and the <height> of the rectangle
+   */
+  rotate () {
+    const temp = this.width;
+
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  /**
+   * multiples the <width> and the <height> of the rectangle by 2
+   */
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
