a8d20e491d39b20fc39a74ed23ab2742d28b00fa
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-11-15 01:12:32 +0000

    fix: add new line to the content of fileC

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -33,7 +33,7 @@ fs.readFile(fileB, 'utf8', (err, dataB) => {
   }
   dataArr.push(dataB);
 
-  fs.writeFile(fileC, dataArr.join('\n'), (err) => {
+  fs.writeFile(fileC, `${dataArr[0]}\n${dataArr[1]}\n`, (err) => {
     if (err) {
       throw err;
     }
