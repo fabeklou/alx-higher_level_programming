9e9bd16713ebf881551c0ebd9c3bf8cc1c0b5c98
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-11-17 23:25:14 +0000

    feat: list states from a database using MySQLdb module

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+This script lists all states from the database hbtn_0e_0_usa
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    user_name = sys.argv[1]
+    user_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db_connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user_name,
+        passwd=user_password,
+        db=db_name,
+        charset="utf8"
+    )
+
+    db_cursor = db_connection.cursor()
+    db_cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    states = db_cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    db_cursor.close()
+    db_connection.close()
