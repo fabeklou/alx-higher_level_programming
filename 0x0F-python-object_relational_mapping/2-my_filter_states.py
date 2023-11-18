162e6e3dba4cd3c8fd9eef8399bb1ab72c59c753
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-11-18 00:28:57 +0000

    feat: find a record in states table using MySQLdb

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""
+This script takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    user_name = sys.argv[1]
+    user_password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db_connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user_name,
+        passwd=user_password,
+        db=db_name,
+        charset="utf8"
+    )
+
+    db_cursor = db_connection.cursor()
+    db_cursor.execute("""
+                        SELECT *
+                        FROM states
+                        WHERE states.name = '{}'
+                        ORDER BY states.id ASC
+                        """.format(state_name))
+    states_by_name = db_cursor.fetchall()
+
+    for state in states_by_name:
+        print(state)
+
+    db_cursor.close()
+    db_connection.close()
