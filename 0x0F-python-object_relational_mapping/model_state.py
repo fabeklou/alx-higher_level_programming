cf3328ba5c2717de1b3efedce9f514fea5298af9
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-11-20 00:39:07 +0000

    feat: implement a state class

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+This module contains the class definition of a State
+and an instance Base = declarative_base()
+"""
+
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    The State class maps <state object> to the <states table>
+
+    Args:
+        __tablename__ (str): name of the mapping table
+        id (int): the unique id
+        name (str): the value, name of a state
+
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
