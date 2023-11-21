0a3cff77cb1d17dee6dacad8f929d4ce01edddb6
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-11-21 18:07:31 +0000

    feat: list states and their cities in ASC order

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""
+This script lists all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa
+"""
+
+
+if __name__ == "__main__":
+    from relationship_state import Base
+    from relationship_state import State
+    from relationship_city import City
+    from sqlalchemy import create_engine
+    from sqlalchemy.engine import URL
+    from sqlalchemy.orm import sessionmaker
+    import sys
+
+    user_name = sys.argv[1]
+    user_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    url_object = URL.create(
+        "mysql",
+        username=user_name,
+        password=user_password,
+        host="localhost",
+        port=3306,
+        database=db_name
+    )
+
+    engine = create_engine(url_object)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).outerjoin(
+                City).order_by(State.id, City.id).all()
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name))
+
+    session.close()
