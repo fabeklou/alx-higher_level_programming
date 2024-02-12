c1e8bb4ccd2504b24245e009eb977688797f2099
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-02-12 23:34:41 +0000

    fix: replace arrow function by normal function

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -6,7 +6,13 @@ const myObject = {
 };
 console.log(myObject);
 
-myObject.incr = () => {
+/**
+ * Regular functions have their own this context.
+ * And this is determined dynamically depending on how you call
+ * or execute the function. Arrow functions, on the other hand,
+ * do not have their own this context.
+ */
+myObject.incr = function () {
   this.value++;
 };
 
