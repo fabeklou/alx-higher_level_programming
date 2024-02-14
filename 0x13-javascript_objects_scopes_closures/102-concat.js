b00526e647096b419603c31d3b32804237a57f29
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-02-14 02:47:04 +0000

    feat: concat files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+
+const dataArr = [];
+
+if (!(fs.existsSync(fileA) &&
+  fs.statSync(fileA).isFile &&
+  fs.existsSync(fileB) &&
+  fs.statSync(fileB).isFile &&
+  fileC !== undefined)) {
+  process.exit();
+}
+
+fs.readFile(fileA, 'utf8', (err, dataA) => {
+  if (err) {
+    throw err;
+  }
+  dataArr.push(dataA);
+});
+
+fs.readFile(fileB, 'utf8', (err, dataB) => {
+  if (err) {
+    throw err;
+  }
+  dataArr.push(dataB);
+
+  fs.writeFile(fileC, dataArr.join(''), (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+});
