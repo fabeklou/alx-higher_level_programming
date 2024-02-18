4946afcc610f639c97cf3dd4e19691c89c210577
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-02-18 20:33:20 +0000

    fix: replace .orm by .ext.declarative

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,13 +5,23 @@ and an instance Base = declarative_base()
 
 """
 
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer
 
 Base = declarative_base()
 
 
 class State(Base):
+    """
+    The State class maps <state objects> to the <states table>
+
+    Args:
+        __tablename__ (str): name of the table
+        id (int): a column/attribute in the table, repr the PK
+        name (str): a column/attribute in the table, repr
+            the name of a state
+
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
