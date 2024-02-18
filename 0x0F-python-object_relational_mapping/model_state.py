166887770eed1bbf56b4ca57b12e9296438dcde8
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-02-18 20:20:24 +0000

    feat: trust the ORM magic

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""This module contains the class definition of a State
+and an instance Base = declarative_base()
+
+"""
+
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, Integer
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
