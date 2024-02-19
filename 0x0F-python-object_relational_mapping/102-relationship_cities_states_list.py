27f6b686b21a587a07c008aca79b599dca8469e8
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-02-19 13:41:04 +0000

    fix: remove extra space in output string

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     cities = session.query(City).join(State).order_by(City.id).all()
 
     for city in cities:
-        print("{}: {}  -> {}".format(
+        print("{}: {} -> {}".format(
             city.id, city.name, city.state.name))
 
     session.close()
