6009accaaa81b422285d1d0f08b11c6ddc6c2940
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-02-29 12:06:58 +0000

    fix: make comment one line and get rid of URL var

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-
-URL=$1
-
-curl -X GET -I "$URL" -s | grep Content-Length | cut -d ' ' -f 2
+# This script takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -X GET -I "$1" -s | grep Content-Length | cut -d ' ' -f 2
