aafaed97704668c1ef8bf10e8addf6e2d7feb7ae
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-02-29 12:49:23 +0000

    fix: follow a redirect and request it

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request to the URL, and displays the body of the response
-test $(curl -X GET -I "$1" -s | grep HTTP/ | cut -d ' ' -f 2) -eq 200 && curl -X GET "$1" -s
+curl -X GET "$1" -sL
