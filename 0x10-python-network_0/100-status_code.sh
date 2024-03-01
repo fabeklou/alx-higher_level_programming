40d040c87b42f2d70d484c3ea4a53a1641159b9f
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-03-01 01:56:46 +0000

    fix: without pip

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -X GET "$1" -s -I | grep HTTP | cut -d ' ' -f 2
+curl -X GET "$1" -s -o  /dev/null --write-out "%{http_code}"
