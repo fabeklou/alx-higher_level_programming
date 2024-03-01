a9c66c440df2a30a8876b0e1fe1cdda4e4cf9f48
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-03-01 00:16:56 +0000

    fix: change delimiter

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL and displays all HTTP methods the server will accept.
-curl -X GET "$1" -sI | grep Allow | cut -d ':' -f 2-
+curl -X GET "$1" -sI | grep Allow | cut -d ' ' -f 2-
