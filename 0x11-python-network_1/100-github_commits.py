facb3c89776ecae21b8adf3ee85e3ddb4d0208d9
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-03-02 02:20:45 +0000

    feat: list 10 commits from a github repo

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""This script takes 2 arguments in order to solve this challenge.
+
+"""
+
+if __name__ == "__main__":
+
+    import requests
+    import sys
+    from pprint import pprint
+
+    repo, owner = sys.argv[1:3]
+    URL = "https://api.github.com/repos/{}/{}/commits".format(repo, owner)
+
+    res = requests.get(URL)
+
+    json_res = res.json()
+
+    json_res.sort(key=lambda x: x["commit"]["author"]["date"], reverse=True)
+
+    for commit in json_res[:10]:
+        sha = commit.get("sha")
+        name = commit.get("commit").get("author").get("name")
+        print("{}: {}".format(sha, name))
