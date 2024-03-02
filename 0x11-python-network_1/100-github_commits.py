9a66389ae6cd37b15bec382d305576bee4404aa8
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-03-02 02:31:49 +0000

    fix: order of params

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,16 +11,16 @@ if __name__ == "__main__":
     from pprint import pprint
 
     repo, owner = sys.argv[1:3]
-    URL = "https://api.github.com/repos/{}/{}/commits".format(repo, owner)
+    URL = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
 
     res = requests.get(URL)
 
-    json_res = res.json()
+    commits = res.json()
 
     # json_res.sort(key=lambda x: x.get("commit").get("author").get("time"),
     #               reverse=True)
 
-    for commit in json_res[:10]:
+    for commit in commits[:10]:
         sha = commit.get("sha")
         name = commit.get("commit").get("author").get("name")
         print("{}: {}".format(sha, name))
