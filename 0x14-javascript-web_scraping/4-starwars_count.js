89884100ddcfeaf1cf7fcd0f9d36c559b6c08432
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-03-27 15:37:34 +0000

    fix: improve logic to work with any url

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,18 +4,18 @@ const request = require('request');
 
 try {
   const URL = process.argv[2];
-  const ID = '18/';
+  const ID = '18';
   request(URL, (error, response, body) => {
     if (error) throw error;
     const DATA = JSON.parse(body);
     let count = 0;
     for (const result of DATA.results) {
       for (const characterLink of result.characters) {
-        const characterId = characterLink.slice(43);
-        if (characterId === ID) count += 1;
+        const tokens = characterLink.split('/');
+        if (tokens[tokens.length - 2] === ID) count += 1;
       }
     }
-    console.log(`${count}`);
+    console.log(count);
   });
 } catch (error) {
   console.error(error);
